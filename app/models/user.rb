9a5baa0be610875042e9816075e0155ef6ffe4b2
gusvicuna <gpvicuna@miuandes.cl>
2019-05-15 10:59:38 -0400

    10 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  	has_many :user_tickets	
-	has_many :tickets, :through => :user_tickets
+  devise :database_authenticatable , :registerable,
+         :recoverable,:rememberable,:trackable,:validatable,
+         :omniauthable ,:omniauth_providers => [:facebook ]
 
-	validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}, uniqueness: true
+  def self.from_omniauth (auth)
+    where(provider: auth.provider,uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.password = Devise.friendly_token [0, 20]
+    end
+  end
+  
+  has_many :user_tickets  
+  has_many :tickets, :through => :user_tickets
 
-	validates :phone, length: {minimum: 9, maximum: 12}, allow_blank: true
-  	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
+  validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}, uniqueness: true
+
+  validates :phone, length: {minimum: 9, maximum: 12}, allow_blank: true
+    validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
 
 
 
