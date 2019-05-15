67442588a6cee3dccb2c6da4d4a963903e344fad
gusvicuna <gpvicuna@miuandes.cl>
2019-05-15 10:37:06 -0400

    Point 5 done

--- db/migrate/20180401225453_create_users.rb
+++ db/migrate/20180401225453_create_users.rb
@@ -3,10 +3,11 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :last_name
-      t.string :email
       t.string :phone
       t.string :password
       t.string :address
+      t.string :provider
+      t.integer :uid
 
       t.timestamps
     end
