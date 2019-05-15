f7b10833dc0d5575d5759781fe998ea9cadd3e1a
gusvicuna <gpvicuna@miuandes.cl>
2019-05-15 11:47:24 -0400

    Add some functionality to buttons

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   root :to => 'sessions#new'
 
+  get '/login', to: 'sessions#new'
   post '/login',   to: 'sessions#create', as: :log_in
   delete '/log_out' => 'sessions#destroy', as: :log_out
 
