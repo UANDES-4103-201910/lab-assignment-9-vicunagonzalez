136c517d6cb7e79cf3d5b6ec8af13c5c6540c6ef
gusvicuna <gpvicuna@miuandes.cl>
2019-05-23 00:19:21 -0400

    add user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
 			user.email = auth.info.email
 			user.name = auth.info.name
 			user.password = Devise.friendly_token[0, 20]
+			user.role = auth.info.role
 		end
 	end
 end
