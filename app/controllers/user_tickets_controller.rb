8bafebfc0ead0d81bd48b118cdec721283ed1412
gusvicuna <gpvicuna@miuandes.cl>
2019-05-23 11:34:21 -0400

    Working... i hate current user

--- app/controllers/user_tickets_controller.rb
+++ app/controllers/user_tickets_controller.rb
@@ -1,10 +1,10 @@
 class UserTicketsController < ApplicationController
   def index
-    @user_tickets = UserTicket.where(user: current_user , paid: true)
+    @user_tickets = UserTicket.where(user: User.find(session['warden.user.user.key'][0][0]) , paid: true)
   end
 
   def new
-    @current_user= current_user
+    @current_user= User.find(session['warden.user.user.key'][0][0])
     @user_ticket = UserTicket.new
   end
 
@@ -13,8 +13,7 @@ class UserTicketsController < ApplicationController
 
     respond_to do |format|
       if @user_ticket.save
-        format.html { redirect_to shopping, notice: 'Ticket was successfully added to shopping cart.' }
-        format.json { render :json }
+        format.html { render :shopping, notice: 'Ticket was successfully added to shopping cart.' }
       else
         format.html { render :new }
         format.json { render json: @user_ticket.errors, status: :unprocessable_entity }
@@ -27,14 +26,8 @@ class UserTicketsController < ApplicationController
   end
 
   def pay
-    respond_to do |format|
-      if @user_ticket.update(user_ticket_params)
-        format.html { redirect_to shopping, notice: 'Tickets successfully purchased.' }
-      else
-        format.html { render shopping }
-        format.json { render json: @user_ticket.errors, status: :unprocessable_entity }
-      end
-    end
+    @user_tickets = UserTicket.where(user: current_user, paid: false)
+    @user_tickets.update_all(paid: true)
   end
   def user_ticket_params
     params.require(:user_ticket).permit(:user_id, :ticket_id, :time, :paid)
